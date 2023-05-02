a26287145bb34b8b875228be98f382d22d3598d5
JKKIM <jkesonjk@gmail.com>
2023-05-02 15:09:49 +0900

    feat: 추가한 enum 타입을 상태와 props에값 적용

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,14 +1,18 @@
+import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 interface Props {
   page: string;
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  // selectedPage: string;
+  selectedPage: SelectedPage;
+  // setSelectedPage: (value: string) => void;
+  setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
   // 소문자로 변환, 빈칸 없애줌
-  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+  // const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
 
   return (
     <AnchorLink
