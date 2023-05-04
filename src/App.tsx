b68c637a3adcfaace738546914be3a8013fb5ccc
JKKIM <jkesonjk@gmail.com>
2023-05-04 14:46:44 +0900

    feat: Home 페이지 타이틀 부분 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -35,6 +36,7 @@ function App() {
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
         />
+        <Home setSelectedPage={setSelectedPage} />
       </div>
     </>
   );
