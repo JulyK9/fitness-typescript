543efbb7a1b43ccbac8803d07869f9eb9a0de62b
JKKIM <jkesonjk@gmail.com>
2023-05-08 17:55:49 +0900

    feat: 타이틀, 본문 부분 애니메이션 추가

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -101,17 +101,35 @@ const Benefits = ({ setSelectedPage }: Props) => {
             {/* TItle */}
             <div className="relative">
               <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
-                <div>
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 1 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 70 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
                   <Htext>
                     MILLIONS OF HAPPY MEMBERS GETTING{" "}
                     <span className="text-primary-500">FIT</span>
                   </Htext>
-                </div>
+                </motion.div>
               </div>
             </div>
 
             {/* Description */}
-            <div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 1 }}
+              variants={{
+                hidden: { opacity: 0, x: -70 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <p className="my-5">
                 Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
                 egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
@@ -126,7 +144,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
                 facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
                 enim mattis odio in risus nunc.
               </p>
-            </div>
+            </motion.div>
 
             {/* Button */}
             <div className="relative mt-16">
