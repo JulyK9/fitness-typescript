9bf7c59c16565991f1e5cf2577679f4b85759b75
JKKIM <jkesonjk@gmail.com>
2023-05-09 15:04:32 +0900

    feat: contactus 페이지 타이틀과 본문 추가

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,11 +1,45 @@
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import Htext from "@/shared/Htext";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  return <div>ContactUs</div>;
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* Header */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, x: -70 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <Htext>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </Htext>
+          <p className="my-5">
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+          </p>
+        </motion.div>
+
+        {/* Form And Image */}
+      </motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
