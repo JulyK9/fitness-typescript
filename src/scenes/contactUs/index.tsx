d6f574318215c01d9ea302d5c67fa9511b2584a7
JKKIM <jkesonjk@gmail.com>
2023-05-09 17:37:41 +0900

    feat: input 요소 및 textarea, button 구현, 텍스트 애니메이션 적용

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,7 +9,8 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
+  px-5 py-3 placeholder-white`;
 
   const {
     register,
@@ -59,16 +60,16 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 1 }}
+            transition={{ duration: 0.5 }}
             variants={{
               hidden: { opacity: 0, y: 50 },
-              visible: { opacity: 1, x: 0 },
+              visible: { opacity: 1, y: 0 },
             }}
           >
             <form
               target="_blank"
               onSubmit={onSubmit}
-              action="https://formsubmit.co/my@gmail.com"
+              action="https://formsubmit.co/edd997db190e0f360cd55560e218ef6d"
               method="POST"
             >
               <input
@@ -87,7 +88,70 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                     "Max length is 100 character."}
                 </p>
               )}
+
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="MESSAGE"
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 character."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
             </form>
+          </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                className="w-full"
+                alt="contact-us-page-graphic"
+                src={ContactUsPageGraphic}
+              />
+            </div>
           </motion.div>
         </div>
       </motion.div>
